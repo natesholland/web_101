4f26029fdb1defcc0245a53841c1508b8c3f060e
Nate Holland <nateh@spiceworks.com>
2016-02-11 23:36:54 -0600

    Troll Ross with some SOAP and add more to the JSON example.

--- web_101.rb
+++ web_101.rb
@@ -21,12 +21,38 @@ get '/format' do
     f.json do
       {
         declaration: 'json is the best',
-        xml: 'is unreadable'
+        foo: 'bar',
+        an_int: 1,
+        null_value: nil,
+        arrays_work_too: [:wiz, :bang, :baz],
+        inception: { hash: 'inside a hash'}
       }.to_json
     end
     f.xml do
       content_type 'text/xml'
       "<foo>bar</foo><xml>sucks</xml>"
     end
+    f.on('application/soap+xml') do
+      content_type 'text/xml'
+      SOAP_STRING
+    end
   end
 end
+
+
+
+private
+
+SOAP_STRING = %q(<?xml version="1.0"?>
+<soap:Envelope
+xmlns:soap="http://www.w3.org/2003/05/soap-envelope/"
+soap:encodingStyle="http://www.w3.org/2003/05/soap-encoding">
+
+<soap:Header>
+  <headers> Some heady information </headers>
+</soap:Header>
+
+<soap:Body>
+  <message>Soap really sucks. Don't use it. </message>
+</soap:Body>
+</soap:Envelope>)
