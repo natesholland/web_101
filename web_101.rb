9e2d12794435424e0c6ecd683397ed88250e11ab
Nate Holland <nateh@spiceworks.com>
2016-02-11 23:22:14 -0600

    Add some formatting and post and put.
    Getting a few more basic web concepts demonstrated here.

--- web_101.rb
+++ web_101.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'sinatra/contrib'
 require 'json'
 
 get '/' do
@@ -7,4 +8,25 @@ end
 
 get '/this/is/a/path' do
   return %q(Good job!)
+end
+
+post '/beam_me_up' do
+  return %q(Very nice. This was a post)
+end
+
+get '/format' do
+  respond_with :index, :name => 'example' do |f|
+    f.txt { 'This is plain text.' }
+    f.html { 'Here is some <strong>html</strong>' }
+    f.json do
+      {
+        declaration: 'json is the best',
+        xml: 'is unreadable'
+      }.to_json
+    end
+    f.xml do
+      content_type 'text/xml'
+      "<foo>bar</foo><xml>sucks</xml>"
+    end
+  end
 end
